4638b00663d606bebea07d3bfe65ffec46e785e7
Eduardo <lalo12345la2@gmail.com>
2022-06-08 23:47:48 -0500

    Se agregaron FK en db

--- app/src/main/java/com/example/transporte/SQLiteOH.java
+++ app/src/main/java/com/example/transporte/SQLiteOH.java
@@ -11,9 +11,9 @@ public class SQLiteOH extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase dbTP) {
-        dbTP.execSQL("CREATE TABLE chofer (nombre VARCHAR(40), numero INT(5), apep VARCHAR(40), apem VARCHAR(40), edad INT(4), curp VARCHAR(20) primary key, localidad VARCHAR(40), direccion VARCHAR(40), telef1 INT(20), telf2 INT(20), licencia VARCHAR(40), tipLice VARCHAR(30))");
-        dbTP.execSQL("CREATE TABLE vehiculo (numero int(5) primary key, placas varchar(15), marca varchar(30), modelo varchar(40), descrip varchar(50))");
-        dbTP.execSQL("CREATE TABLE pago (id varchar(35) primary key, chofer varchar(20), comentarios varchar(50), ano int(9), mes int(9), pago1 DECIMAL(10,2), pago2 DECIMAL(10,2), pago3 DECIMAL(10,2), pago4 DECIMAL(10,2), pago5 DECIMAL(10,2))");
+        dbTP.execSQL("CREATE TABLE chofer (nombre VARCHAR(40), numero INT(5), apep VARCHAR(40), apem VARCHAR(40), edad INT(4), curp VARCHAR(20) NOT NULL PRIMARY KEY, localidad VARCHAR(40), direccion VARCHAR(40), telef1 INT(20), telf2 INT(20), licencia VARCHAR(40), tipLice VARCHAR(30), FOREIGN KEY(numero) REFERENCES vehiculo(numero))");
+        dbTP.execSQL("CREATE TABLE vehiculo (numero int(5) NOT NULL PRIMARY KEY, placas varchar(15) NOT NULL, marca varchar(30), modelo varchar(40), descrip varchar(50))");
+        dbTP.execSQL("CREATE TABLE pago (id varchar(35) NOT NULL PRIMARY KEY, chofer varchar(20), comentarios varchar(50), ano int(9), mes int(9), pago1 DECIMAL(10,2), pago2 DECIMAL(10,2), pago3 DECIMAL(10,2), pago4 DECIMAL(10,2), pago5 DECIMAL(10,2), FOREIGN KEY (chofer) REFERENCES chofer(curp))");
     }
 
     @Override
